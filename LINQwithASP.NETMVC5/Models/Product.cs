1c0014d1e1a6aa1efcde0e857a84cd82f6796534
hieupvd00585 <hieupvd00585@fpt.edu.vn>
2018-12-02 09:30:57 +0700

    hoàn thành

---
+++ LINQwithASP.NETMVC5/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace LINQwithASP.NETMVC5.Models
+{
+    public class Product
+    {
+        [Key]
+       public int Id { get; set; }
+        public string Name { get; set; }
+        public string Qty { get; set; }
+        public string Price { get; set; }
+        public DateTime EntryDate { get; set; }
+    }
+}
